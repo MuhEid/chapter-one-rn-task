bbb470fc36ef33ca8df8568bbeb7d724dbd0838b
MuhEid96 <moh.eid.1996@gmail.com>
2025-10-30 20:39:31 +0300

    Add priority and creation date fields to Task type definition

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,5 +1,9 @@
+export type Priority = "high" | "medium" | "low";
+
 export type Task = {
     id: string;
     title: string;
     done: boolean;
+    createdAt: number; // Date.now() at creation
+    priority: Priority;
 };
